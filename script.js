4d3b923ad78ef7b417eef3574a05b5cf6828cf0b
MouNiR-X-Dev <qmounir.bouamarp@gmail.com>
2025-03-08 11:43:43 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var stripe = Stripe('YOUR_PUBLIC_KEY');
-var checkoutButton = document.getElementById('checkout-button');
-checkoutButton.addEventListener('click', function() {
-    stripe.redirectToCheckout({
-        sessionId: 'YOUR_SESSION_ID'
-    });
-});
